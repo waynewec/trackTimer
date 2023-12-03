51620a0f7cdc88308b910d22d8a15f408d41e9a9
wayne <waynewec@gmail.com>
2023-12-03 12:07:52 -0700

    Working Makefile build for feather32u4

---
+++ gate/gate.ino
@@ -0,0 +1,17 @@
+#define DECODE_FAST
+
+#include <IRremote.hpp>
+
+void setup() {
+    Serial.begin(115200);
+
+    //IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+}
+
+void loop() {
+    //IrReceiver.RECEIVE???();
+
+    //if (IrReceiver.decodedIRData.command == STARTSTOP;){
+        //doTheThing();
+   //}
+}
